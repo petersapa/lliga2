870b7783faa847d8d1249bff8e8e7555f919202d
Sánchez Miranda <psanchez@sapalomera.net>
2025-05-21 17:51:18 +0200

    millores 1

--- lliga/admin.py
+++ lliga/admin.py
@@ -2,32 +2,47 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Lliga)
-admin.site.register(Equip)
-admin.site.register(Jugador)
+@admin.register(Lliga)
+class LligaAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'temporada', 'data_inici', 'data_fi')
+    search_fields = ('nom', 'temporada')
+
+@admin.register(Equip)
+class EquipAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'ciutat', 'fundacio')
+    search_fields = ('nom', 'ciutat')
+
+@admin.register(Jugador)
+class JugadorAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'cognoms', 'dorsal', 'posicio', 'equip')
+    search_fields = ('nom', 'cognoms', 'equip__nom')
+    list_filter = ('posicio', 'nacionalitat')
+    autocomplete_fields = ['equip']
 
 class EventInline(admin.TabularInline):
     model = Event
-    fields = ["temps","tipus","jugador","equip"]
+    fields = ["temps", "tipus", "jugador", "equip", "detalls"]
     ordering = ("temps",)
+    extra = 1
 
+@admin.register(Partit)
 class PartitAdmin(admin.ModelAdmin):
-    # podem fer cerques en els models relacionats
-    # (noms dels equips o títol de la lliga)
-    search_fields = ["local__nom","visitant__nom","lliga__nom"]
-    # el camp personalitzat ("resultats" o recompte de gols)
-    # el mostrem com a "readonly_field"
-    readonly_fields = ["resultat",]
-    list_display = ["local","visitant","resultat","lliga","data"]
+    search_fields = ["local__nom", "visitant__nom", "lliga__nom"]
+    readonly_fields = ["resultat"]
+    list_display = ["local", "visitant", "resultat", "lliga", "data", "finalitzat"]
+    list_filter = ("lliga", "finalitzat", "data")
     ordering = ("-data",)
-    inlines = [EventInline,]
-    def resultat(self,obj):
-        gols_local = obj.events.filter(
-                        tipus="GOL",
-                        equip=obj.local).count()
-        gols_visit = obj.events.filter(
-                        tipus="GOL",
-                        equip=obj.visitant).count()
-        return "{} - {}".format(gols_local,gols_visit)
+    inlines = [EventInline]
+    autocomplete_fields = ['lliga', 'local', 'visitant']
 
-admin.site.register(Partit,PartitAdmin)
+    def resultat(self, obj):
+        gols_local = obj.events.filter(tipus="GOL", equip=obj.local).count()
+        gols_visit = obj.events.filter(tipus="GOL", equip=obj.visitant).count()
+        return f"{gols_local} - {gols_visit}"
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('partit', 'temps', 'tipus', 'jugador', 'equip')
+    list_filter = ('tipus', 'equip', 'partit__lliga')
+    search_fields = ('jugador__nom', 'jugador__cognoms', 'equip__nom')
+    autocomplete_fields = ['partit', 'jugador', 'equip']
